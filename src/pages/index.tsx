47933c091cede50c8ef944647c707b16aacfb57e
Sardor <ozdemeir09@gmail.com>
2024-02-15 21:34:29 +0500

    SEO & nprogress

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,30 @@
-import axios from 'axios';
-import { GetServerSideProps } from 'next';
-import { MenuItem } from '../interfaces/menu.interface';
-import { withLayout } from '../layout/layout';
-import { HomePageComponent } from '../page-components';
+import axios from "axios";
+import { GetServerSideProps } from "next";
+import { MenuItem } from "../interfaces/menu.interface";
+import { withLayout } from "../layout/layout";
+import { HomePageComponent } from "../page-components";
+import Seo from "../layout/seo/seo";
 
 const Index = (): JSX.Element => {
-	return <HomePageComponent />;
+  return (
+    <Seo>
+      <HomePageComponent />
+    </Seo>
+  );
 };
 
 export default withLayout(Index);
 
 export const getServerSideProps: GetServerSideProps<HomePageProps> = async () => {
-	const { data: menu } = await axios.post<MenuItem[]>(`${process.env.NEXT_PUBLIC_DOMAIN}/api/page-find`, { firstCategory: 0 });
+  const { data: menu } = await axios.post<MenuItem[]>(`${process.env.NEXT_PUBLIC_DOMAIN}/api/page-find`, { firstCategory: 0 });
 
-	return {
-		props: {
-			menu,
-		},
-	};
+  return {
+    props: {
+      menu,
+    },
+  };
 };
 
 interface HomePageProps {
-	menu: MenuItem[];
+  menu: MenuItem[];
 }
